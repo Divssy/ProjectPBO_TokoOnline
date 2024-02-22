62579e47c8b467189bef0290a674a3945805bc96
Divssy <divya_alfarina_32rpl@student.smktelkom-mlg.sch.id>
2024-02-23 00:59:21 +0700

    first commit

---
+++ Main.java
@@ -0,0 +1,25 @@
+
+package javaapplication1;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+  
+        Member member;
+        member = new Member() {};
+        Karyawan karyawan = new Karyawan();
+        Barang barang = new Barang();
+        Transaksi transaksi = new Transaksi();
+        Laporan laporan = new Laporan();
+
+        // Proses transaksi
+        transaksi.prosesTransaksi(member, transaksi, barang);
+
+        // Menampilkan laporan
+        laporan.laporan(barang);
+        laporan.laporan(member);
+        laporan.laporan(transaksi, barang);
+    }
+}
+
